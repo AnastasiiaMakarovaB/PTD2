ff180468160e987f13da2593620bdb11d39bbbd8
Anastasiia <barsaaus@gmail.com>
2025-08-28 15:21:25 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,19 @@
 public class Main {
     public static void main(String[] args) {
-    int initialBalance = 100;   // начальный баланс
-    int deposits = 2700;        // сумма пополнения
+        int initialBalance = 100;   // начальный баланс
+        int deposits = 2700;        // сумма пополнения
 
-    int bonus; // бонусные рубли
-    if (deposits > 1000) {
+        int bonus; // бонусные рубли
         bonus = deposits / 100;
-    } else {
-        bonus = 0;
+        if (deposits > 1000) {
+            bonus = deposits / 100;
+        } else {
+            bonus = 0;
+        }
+
+        int finalBalance = initialBalance + deposits + bonus;
+
+        System.out.println("Бонусные рубли: " + bonus);
+        System.out.println("Итоговый счёт: " + finalBalance);
     }
-
-    int finalBalance = initialBalance + deposits + bonus;
-
-    System.out.println("Бонусные рубли: " + bonus);
-    System.out.println("Итоговый счёт: " + finalBalance);
-}
 }
